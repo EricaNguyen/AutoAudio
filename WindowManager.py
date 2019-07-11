3af14e7af1ffaabcd928731246b47f7b84b797f1
EricaNguyen <39079011+EricaNguyen@users.noreply.github.com>
2019-07-11 14:41:39 -0700

    WindowManager now has buttons. A bit buggy

--- WindowManager.py
+++ WindowManager.py
@@ -1,5 +1,13 @@
 from tkinter import *
 import tkinter as tk
+import os
+import signal
+import subprocess
+
+#p = subprocess.Popen(['head', 'README.txt'])
+#command definitions
+#record button functions, aka button
+bool = 0
 
 class App(tk.Frame):
     def __init__(self, master=None): #where info on what the window displays is defined
@@ -10,6 +18,28 @@ class App(tk.Frame):
         controlPanel = LabelFrame(self, text = 'Control Panel') 
         controlPanel.config(bg='lightgreen', height = '200', width = '400') 
         controlPanel.pack(side = LEFT)
+        
+        #buttons
+        button = Button(controlPanel, text="Start Recording", command = self.record)
+        button.place(x=20, y=20, anchor="w")
+        button2 = Button(controlPanel, text="Stop Recording", command = self.stopR)
+        button2.place(x=20, y=40, anchor="w")
+        button3 = Button(controlPanel, text="Quit", command = self.quit)
+        button3.place(x=20, y=60, anchor="w")
+
+        #T = Text(master, height=1, width=20)
+        #T.pack();
+        #T.insert(END, "Put File Name Here")
+
+        #Label of fileName entry field
+        Label(controlPanel, text="Put Output Filename Here").place(x=20, y=90, anchor="w")
+        e1 = Entry(controlPanel)
+        e1.place(x=20, y=80, anchor="w")
+        #default name
+        fileName = "NoteSheet";
+        #label for recording/not recording
+        label = Label(controlPanel, text="Not Recording", font=("Helvetica", 12), fg="black")
+        label.place(x=20, y=100, anchor="w")
         
         #makes an instructions panel within the window.
         instrPanel = LabelFrame(self, text = 'Instructions go here') 
@@ -30,11 +60,47 @@ class App(tk.Frame):
         scrollbar.pack( side = RIGHT, fill = Y )
         #output listed here
         mylist = Listbox(outputPanel, yscrollcommand = scrollbar.set )
+        #mylist = Text(outputPanel, yscrollcommand = scrollbar.set )
         for line in range(100): #replace with music note output
            mylist.insert(END, "This is line number " + str(line))
         mylist.config(height = '400', width = '800')
         mylist.pack( side = LEFT, fill = BOTH )
         scrollbar.config( command = mylist.yview )
+        
+    def record(self):
+        global p
+        global bool
+        #T.delete('1.0', END)
+        #T.insert(END,"Recording")
+        label = Label(self, text="Not Recording", font=("Helvetica", 12), fg="black")
+        label.place(x = 40, y = 120)
+        label.config(text="Recording")
+        p = subprocess.Popen(['python', 'freqAnalyzer.py'])
+        #test code, use instead of subprocess if thing don't work
+        #p = subprocess.Popen(['python', 'runTheThing.py'])
+        bool = 1;
+        #print(fileName)
+        
+    #stop record button functions, aka button 2
+    def stopR(self):
+        global fileName
+        global bool
+        p.kill()
+        #T.delete('1.0', END)
+        #T.insert(END,"Not Recording")
+        label = Label(self, text="Not Recording", font=("Helvetica", 12), fg="black")
+        label.place(x = 40, y = 120)
+        label.config(text="Not Recording")
+        fileName = e1.get()
+        #check if there was any recording done
+        if bool == 1:
+            #pass filename into lilypond or freqAnalyzer
+            bool = 0
+        
+
+    #quitting the program, aka button 3
+    def quit(self):
+        exit()
         
 # create the application
 myapp = App()
