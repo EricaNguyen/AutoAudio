56c1195cbcd1d17ff3447e1f986bdda959b99bfb
wdxu <wdxu@ucsc.edu>
2019-07-11 02:15:52 -0700

    adding runTheThing gui file,WILL NOT WORK IF freqAnalyzer ISN'T WORKING

---
+++ runTheThing.py
@@ -0,0 +1,37 @@
+from tkinter import *
+import subprocess
+
+#import freqAnalyzer
+
+#command definitions
+def record():
+	T.delete('1.0', END)
+	T.insert(END,"Recording")
+	p = subprocess.Popen(['python', 'freqAnalyzer.py'])
+	#test code, use instead of subprocess if thing don't work
+	#p = subprocess.Popen('ls')
+	
+def stopR():
+	#will not work with test code, as p will terminate on its own very quick,
+	p.terminate
+	T.delete('1.0', END)
+	T.insert(END,"Not Recording")
+
+def quit():
+	exit()
+	
+master = Tk()
+frame = Frame(master)
+frame.pack()
+#button definitions
+button = Button(master, text="Start Recording", command = record)
+button.pack(side=LEFT)
+button2 = Button(master, text="Stop Recording", command = stopR)
+button2.pack(side=LEFT)
+button3 = Button(master, text="Quit", command = quit)
+button3.pack(side=LEFT)
+T = Text(master, height=1, width=14)
+T.pack();
+T.insert(END, "Not Recording")
+
+mainloop()
