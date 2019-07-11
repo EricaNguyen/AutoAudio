6e653cbd1dc44b8fcbffa4d689b25b278abf5b7f
wdxu <wdxu@ucsc.edu>
2019-07-11 13:36:38 -0700

    altered look of basics

--- runTheThing.py
+++ runTheThing.py
@@ -19,10 +19,13 @@ def record():
 	
 #stop record button functions, aka button 2
 def stopR():
+	global fileName
 	p.kill()
-	T.delete('1.0', END)
+	#T.delete('1.0', END)
 	#T.insert(END,"Not Recording")
 	label.config(text="Not Recording")
+	fileName = e1.get()
+	
 
 #quitting the program, aka button 3
 def quit():
@@ -31,18 +34,26 @@ def quit():
 master = Tk()
 master.title("AutoAudio")
 frame = Frame(master)
-frame.pack()
+frame.grid()
 #button definitions
 button = Button(master, text="Start Recording", command = record)
-button.pack(side=LEFT)
+button.grid(row=0)
 button2 = Button(master, text="Stop Recording", command = stopR)
-button2.pack(side=LEFT)
+button2.grid(row=1)
 button3 = Button(master, text="Quit", command = quit)
-button3.pack(side=LEFT)
-T = Text(master, height=1, width=20)
-T.pack();
-T.insert(END, "Put File Name Here")
+button3.grid(row=2)
+
+#T = Text(master, height=1, width=20)
+#T.pack();
+#T.insert(END, "Put File Name Here")
+
+#Label of fileName entry field
+Label(master, text="Output File Name").grid(row=0, column=1)
+e1 = Entry(master)
+e1.grid(row=1, column=1)
+#default name
+fileName = "NoteSheet";
+#label for recording/not recording
 label = Label(master, text="Not Recording", font=("Helvetica", 12), fg="black")
-label.pack()
-mainloop(
-)
+label.grid(row=2, column=1)
+mainloop()
