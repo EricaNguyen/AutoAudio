c960b4989af4933923fe145428d266aa0e9b0395
wdxu <wdxu@ucsc.edu>
2019-07-11 13:49:07 -0700

    altered look of basics

--- runTheThing.py
+++ runTheThing.py
@@ -8,23 +8,32 @@ import subprocess
 p = subprocess.Popen(['head', 'README.txt'])
 #command definitions
 #record button functions, aka button
+bool = 0;
 def record():
 	global p
+	global bool
 	#T.delete('1.0', END)
 	#T.insert(END,"Recording")
 	label.config(text="Recording")
-	#p = subprocess.Popen(['python', 'freqAnalyzer.py'])
+	p = subprocess.Popen(['python', 'freqAnalyzer.py'])
 	#test code, use instead of subprocess if thing don't work
 	#p = subprocess.Popen(['python', 'runTheThing.py'])
+	bool = 1;
+	#print(fileName)
 	
 #stop record button functions, aka button 2
 def stopR():
 	global fileName
+	global bool
 	p.kill()
 	#T.delete('1.0', END)
 	#T.insert(END,"Not Recording")
 	label.config(text="Not Recording")
 	fileName = e1.get()
+	#check if there was any recording done
+	if bool == 1:
+		#pass filename into lilypond or freqAnalyzer
+		bool = 0;
 	
 
 #quitting the program, aka button 3
@@ -48,7 +57,7 @@ button3.grid(row=2)
 #T.insert(END, "Put File Name Here")
 
 #Label of fileName entry field
-Label(master, text="Output File Name").grid(row=0, column=1)
+Label(master, text="Put Output Filename Here").grid(row=0, column=1)
 e1 = Entry(master)
 e1.grid(row=1, column=1)
 #default name
