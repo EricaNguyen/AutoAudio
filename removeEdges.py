e8f11fa0b871a3dcbbbbf4abad16d51ff3c05eb8
MaxShinnerl <mhshinne@ucsc.edu>
2019-07-18 15:42:37 -0700

    added pitch checker to rest removal

--- removeEdges.py
+++ removeEdges.py
@@ -1,7 +1,9 @@
 #remove rests at start and beginning of recording
 
 def removeEdges(noteList):
-   del noteList[0]
-   del noteList[len(noteList)-1]
+    if noteList[0].pitch == 'r':
+        del noteList[0]
+    if noteList[len(noteList)-1].pitch == 'r':
+        del noteList[len(noteList)-1]
 
-   return noteList
+    return noteList
