89ae44f5c13cce6c15bb37baf81da7cddc89c007
mcao9 <mcao9@ucsc.edu>
2019-07-20 14:12:05 -0700

    removed files

--- scripts/lilyP.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo creating pdf, wait a minute...
-lilypond output.ly
-echo pdf done
-
-echo moving pdf to folder
-
-mkdir ~/Desktop/AutoAudioOutput
-mv output.pdf ~/Desktop/AutoAudioOutput
