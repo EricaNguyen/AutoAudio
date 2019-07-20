bec0d5519574cf16e14b1a32a7e50ea6d5e7c72d
mcao9 <mcao9@ucsc.edu>
2019-07-20 00:30:50 -0700

    removed files

--- noteClass.py
+++
@@ -1,9 +0,0 @@
-class Note(object):
-    def __init__(self, pitch, duration, durationLength, soundPressureLevel):
-        self.pitch = pitch
-        self.duration = duration
-        self.soundPressureLevel = soundPressureLevel
-        self.durationLength = durationLength
-
-    def printNote(self):
-        print("Pitch: ", self.pitch, "| Duration :", self.duration, "| Duration Note:", self.durationLength, "| Sound Pressure Level:", self.soundPressureLevel)
--- removeEdges.py
+++
@@ -1,9 +0,0 @@
-#remove rests at start and beginning of recording
-
-def removeEdges(noteList):
-    if noteList[0].pitch == 'r':
-        del noteList[0]
-    if noteList[len(noteList)-1].pitch == 'r':
-        del noteList[len(noteList)-1]
-
-    return noteList
